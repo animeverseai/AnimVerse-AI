adf58d92acd7945286d6c3bb83178d044bcd0610
animeverseai <khanmaksud1234@gmail.com>
2026-06-30 16:32:03 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAlq12gxPLIUGSl8yEa0Rp9vgnv6ySxJJU",
+  authDomain: "animverse-ai.firebaseapp.com",
+  projectId: "animverse-ai",
+  storageBucket: "animverse-ai.firebasestorage.app",
+  messagingSenderId: "528665034632",
+  appId: "1:528665034632:web:ab2d92587f782d1287c43d"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
