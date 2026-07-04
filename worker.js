eda3300d233c46db77d219a0ef75d2dc7de9bd1e
animeverseai <khanmaksud1234@gmail.com>
2026-07-04 14:52:27 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,17 @@
 export default {
   async fetch(request, env) {
 
-    return new Response("AnimVerse AI Worker is Running ✅");
+    return new Response(
+      JSON.stringify({
+        status: "success",
+        message: "AnimVerse AI Worker Connected Successfully"
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
 
   }
 }
